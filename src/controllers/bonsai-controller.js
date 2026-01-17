24d4ea8eaa0716467e49ee7752461758caf06a17
DarenVejarano <darenandrevejarano@paucasesnovescifp.cat>
2026-01-17 16:49:17 +0100

    Task 1: Estructura del proyecto Bonsai

---
+++ src/controllers/bonsai-controller.js
@@ -0,0 +1,6 @@
+// Controlador de Bonsáis (estructura inicial para Task 1)
+
+exports.listarTodos = (req, res) => {};
+exports.obtenerPorId = (req, res) => {};
+exports.buscar = (req, res) => {};
+exports.calcular = (req, res) => {};
